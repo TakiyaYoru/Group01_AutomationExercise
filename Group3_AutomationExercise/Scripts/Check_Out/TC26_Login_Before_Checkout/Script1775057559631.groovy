41192d66be3123b0fd44ad8c304fda0782e9146b
Duy Kha <137805552+TakiyaYoru@users.noreply.github.com>
2026-03-29 11:11:00 +0700

    Add TC26 login-before-checkout case and script

---
+++ Group3_AutomationExercise/Scripts/Check_Out/TC26_Login_Before_Checkout/Script1775057559631.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+// login
+WebUI.navigateToUrl(GlobalVariable.baseUrl + '/login')
+WebUI.setText(findTestObject('Authentication/txt_Login_Email'), GlobalVariable.validEmail)
+WebUI.setText(findTestObject('Authentication/txt_Login_Password'), GlobalVariable.validPassword)
+WebUI.click(findTestObject('Authentication/btn_Login'))
+
+// add product
+WebUI.navigateToUrl(GlobalVariable.baseUrl + '/products')
+WebUI.scrollToElement(findTestObject('Cart/card_First_Product'), 5)
+WebUI.mouseOver(findTestObject('Cart/card_First_Product'))
+WebUI.jsClick(findTestObject('Cart/btn_Add_To_Cart_First_Product'))
+
+WebUI.click(findTestObject('Cart/btn_View_Cart'))
+
+// checkout
+WebUI.click(findTestObject('Checkout/btn_Proceed_To_Checkout'))
+
+// place order
+WebUI.click(findTestObject('Checkout/btn_Place_Order'))
+
+// payment
+WebUI.setText(findTestObject('Checkout/txt_Card_Name'), GlobalVariable.cardName)
+WebUI.setText(findTestObject('Checkout/txt_Card_Number'), GlobalVariable.cardNumber)
+WebUI.setText(findTestObject('Checkout/txt_CVC'), GlobalVariable.cardCVC)
+WebUI.setText(findTestObject('Checkout/txt_Expiry_Month'), '12')
+WebUI.setText(findTestObject('Checkout/txt_Expiry_Year'), '2030')
+
+WebUI.click(findTestObject('Checkout/btn_Pay_Confirm'))
+
+// verify
+WebUI.verifyElementPresent(findTestObject('Checkout/lbl_Order_Success'), 10)
+
+WebUI.closeBrowser()
